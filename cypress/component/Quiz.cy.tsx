b6eb637a8df4107f20cf370c0d53d66b3e376695
Lauren Lambert <lambertelauren@gmail.com>
2025-05-03 19:06:25 -0500

    bug fix

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -1,41 +1,52 @@
-import { mount } from "cypress/react";
-import Quiz from "../../client/src/components/Quiz";
+import { mount } from 'cypress/react';
+import Quiz from '../../client/src/components/Quiz';
 
 describe("Quiz Component", () => {
   beforeEach(() => {
     cy.intercept(
-      {
-        method: "GET",
-        url: "api/questions/random",
-      },
-      {
-        fixture: "questions.json",
-        statusCode: 200,
-      }
+      "GET",
+      "/api/questions/random",
+      { fixture: "questions.json", statusCode: 200 }
     ).as("getRandomQuestion");
   });
-  it("should start the quiz and and display the first question", () => {
+
+  it("should start the quiz and display the first question", () => {
     mount(<Quiz />);
     cy.get("button").contains("Start Quiz").click();
     cy.get(".card").should("be.visible");
     cy.get("h2").should("not.be.empty");
   });
 
-  it("should answer questions and complete the quiz", () => {
+  it("should answer all questions and display the score", () => {
     mount(<Quiz />);
     cy.get("button").contains("Start Quiz").click();
-    cy.get("button").contains("1").click();
-    cy.get(".alert-success")
-    .should("be.visible")
-    .invoke("text")
-    .should("include", "Your score");  });
 
-  it("should start the quiz again after completion", () => {
+    cy.fixture("questions.json").then((questions) => {
+      questions.forEach(() => {
+        cy.get("button").contains("1").click();
+        cy.get("h2").should("not.be.empty");
+      });
+
+      cy.get(".alert-success", { timeout: 8000 })
+        .should("be.visible")
+        .invoke("text")
+        .should("include", "Your score");
+    });
+  });
+
+  it("should restart the quiz after completion", () => {
     mount(<Quiz />);
     cy.get("button").contains("Start Quiz").click();
-    cy.get("button").contains("1").click();
-    cy.get("button").contains("Take New Quiz").click();
-    cy.get(".card").should("be.visible");
-    cy.get("h2").should("not.be.empty");
+
+    cy.fixture("questions.json").then((questions) => {
+      questions.forEach(() => {
+        cy.get("button").contains("1").click();
+        cy.get("h2").should("not.be.empty");
+      });
+
+      cy.contains("Take New Quiz", { timeout: 8000 }).should("be.visible").click();
+      cy.get(".card").should("be.visible");
+      cy.get("h2").should("not.be.empty");
+    });
   });
 });
